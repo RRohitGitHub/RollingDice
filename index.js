0984570ea7b611bb6f1680600ab93f509d789514
Rohit Parab <rohitparab213@gmail.com>
2021-05-09 12:00:39 +0530

    Display text and switch between players

--- index.js
+++ index.js
@@ -18,4 +18,20 @@ const reset = document.getElementById("resetBtn");
 
 document.getElementById("rollBtn").addEventListener("click",function(){
     const random = Math.floor(Math.random()*6) + 1;
+    if(player1Turn){
+        player1Dice.textContent = random;
+        player1Turn = false;
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+        message.textContent = "Player 2 Turn"
+        
+    }
+    else{
+        player2Dice.textContent = random;
+        player1Turn = true;
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+        message.textContent = "Player 1 Turn"
+     
+    }
 })
